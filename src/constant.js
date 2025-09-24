f9a8733c09944f39a79472344bb29aa436fdc780
gitHritik <97736648+gitHritik@users.noreply.github.com>
2025-09-24 17:21:11 +0530

    new one

--- src/constant.js
+++ src/constant.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 import Cookies from "js-cookie";
 
-export const BACKEND_URL = "https://ticket-less-backend-7vfq.vercel.app";
+export const BACKEND_URL = "https://ticket-less-backend-bcll.vercel.app";
 export const USERS_URL = "/api/users";
-export const BASE_URL = "https://ticket-less-backend-7vfq.vercel.app";
+export const BASE_URL = "https://ticket-less-backend-bcll.vercel.app";
 
 const getJwtToken = () => {
   return Cookies.get("jwt"); // Replace 'jwt_token' with the name of your cookie
